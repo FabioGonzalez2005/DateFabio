3ce7e1f8ef6fa1789528a65027980bd23dc351c4
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-04-02 12:52:52 +0100

    Añadido mainDate

---
+++ mainDate.py
@@ -0,0 +1,9 @@
+from date import Date
+
+def main():
+    print("Días en febrero de 2020:", Date.days_in_month(5, 2021))
+
+    date1 = Date(1, 1, 2020)
+    print("Días transcurridos desde el 1-1-1900 hasta el 1-1-2020:", date1.get_delta_days())
+
+main()
